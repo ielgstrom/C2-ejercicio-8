cdfdaa1ec6a197d542028e114c98d1ceaf9b0fb3
Michaelsp11 <michaelsalgadop11@gmail.com>
2021-06-02 21:01:14 +0200

    Herencias de personajes + añadido parametro serie

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-classes-per-file */
 class Personaje {
     nombre;
 
@@ -6,6 +7,8 @@ class Personaje {
     estado;
 
     edad;
+
+    serie = "Juego de Tronos";
 
     constructor(nombre, familia, edad, estado = "vivo") {
         this.nombre = nombre;
@@ -20,5 +23,107 @@ class Personaje {
 
     morir() {
         this.estado = "muerto";
+    }
+}
+class Rey extends Personaje {
+    anyosReinado;
+
+    constructor(nombre, familia, edad, anyosReinado, estado = "vivo") {
+        super(nombre, familia, edad, estado);
+        this.anyosReinado = anyosReinado;
+    }
+
+    comunicar() {
+        return super.comunicar("Vais a morir todos.");
+    }
+}
+class Luchador extends Personaje {
+    arma;
+
+    destreza;
+
+    constructor(nombre, familia, edad, arma, destreza, estado = "vivo") {
+        super(nombre, familia, edad, estado);
+        this.arma = arma;
+        this.destreza = this.getDestreza(destreza);
+    }
+
+    comunicar() {
+        return super.comunicar("Primero pego y luego pregunto.");
+    }
+
+    getDestreza(valor) {
+        if (valor < 0) {
+            return 0;
+        } else if (valor > 10) {
+            return 10;
+        } else {
+            return valor;
+        }
+    }
+}
+class Asesor extends Personaje {
+    personajeAsesorado;
+
+    constructor(nombre, familia, edad, personajeAsesorado, estado = "vivo") {
+        super(nombre, familia, edad, estado);
+        if (this.comprobarPersonajeAsesorado(personajeAsesorado)) {
+            this.personajeAsesorado = personajeAsesorado;
+        } else {
+            throw `El asesor (${this.nombre}) no puede asesorar al personaje (${personajeAsesorado.nombre}). Solo puede asesorar reyes, luchadores, asesores o escuderos`;
+        }
+    }
+
+    comunicar() {
+        return super.comunicar("No sé por qué, pero creo que voy a morir pronto.");
+    }
+
+    comprobarPersonajeAsesorado(personaje) {
+        return (
+            personaje instanceof Rey ||
+            personaje instanceof Luchador ||
+            personaje instanceof Asesor ||
+            personaje instanceof Escudero
+        );
+    }
+}
+class Escudero extends Personaje {
+    personajeAlQueSirve;
+
+    gradoPelotismo;
+
+    constructor(
+        nombre,
+        familia,
+        edad,
+        personajeAlQueSirve,
+        gradoPelotismo,
+        estado = "vivo"
+    ) {
+        super(nombre, familia, edad, estado);
+        if (this.comprobarPersonajeAlQueSirve(personajeAlQueSirve)) {
+            this.personajeAlQueSirve = personajeAlQueSirve;
+        } else {
+            throw `El escudero (${this.nombre}) no puede servir al personaje (${personajeAlQueSirve.nombre}) porque no es un luchador.`;
+        }
+        this.gradoPelotismo = this.getGradoPelotismo(gradoPelotismo);
+    }
+
+    comunicar() {
+        return super.comunicar("Soy un loser.");
+    }
+
+    comprobarPersonajeAlQueSirve(personaje) {
+        return personaje instanceof Luchador;
+    }
+
+    getGradoPelotismo(valor) {
+        if (valor < 0) {
+            return 0;
+        } else if (valor > 10) {
+            return 10;
+        } else {
+            return valor;
+        }
     }
 }
