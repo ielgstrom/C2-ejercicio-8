ad6100dc7af61ad4d2e8545f90a7dd339ad798c7
Michaelsp11 <michaelsalgadop11@gmail.com>
2021-06-02 18:32:43 +0200

    Método morir agregado, método hablar cambiado a comunicarse, cambiado orden parametros constructor

--- js/index.js
+++ js/index.js
@@ -1,15 +1,24 @@
 class Personaje {
-  nombre;
-  familia;
-  estado;
-  edad;
-  constructor(nombre, familia, estado = "vivo", edad) {
-    this.nombre = nombre;
-    this.familia = familia;
-    this.estado = estado;
-    this.edad = edad;
-  }
-  hablar(mensaje) {
-    return `${mensaje}`;
-  }
+    nombre;
+
+    familia;
+
+    estado;
+
+    edad;
+
+    constructor(nombre, familia, edad, estado = "vivo") {
+        this.nombre = nombre;
+        this.familia = familia;
+        this.estado = estado;
+        this.edad = edad;
+    }
+
+    comunicar(mensaje) {
+        return `${this.nombre} dice: ${mensaje}`;
+    }
+
+    morir() {
+        this.estado = "muerto";
+    }
 }
